520f01ed333f4e68593f8a945e352b342fb281fc
TT-420 <tomas.tesar@skolakrizik.cz>
2025-04-27 23:19:18 +0200

    Add files via upload

--- Moje-hra.cpp
+++ Moje-hra.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void rozdelovac(){
+  cout << "________________________________________________________________________________________________________________________\n";
+
+}
+void rozcesti(){
+     int vyber2;
+     cout << "1- muzes jit po ceste doprava smer mesto.\n";
+     cout << "2- muzes jit rovne smer bazina.\n";
+     cout << "3- muzes jit do leva smer cerne skaly.\n";
+     cin >> vyber2;
+     }
+
 int main(){
   cout<< "vitam te v tomto svete jmenem tralalelo tralala\n";
  int vyber=0, spokojenost;
@@ -28,7 +40,8 @@ int main(){
       cout << "Bambal muze jednou za 3 tahy snizit silu utoku jeho nepritele o 80% na jedno kolo (stoji 8 many)\n";
       cout << "Bambal muze jednou za pet kol aktivovat stit, tento stit se znici na jednu ranu ale ta rana neubere ani 1 hp Bambalovi (stoji 10 many)\n";
       cout << "Bambal se muze ovsem behem hry naucit nove kouzla ci vylepsit ty zakladni\n";
-      break;
+       rozdelovac();
+    break;
   case 2:
       cout << "Vybrali jste postavu Hromotluk";
       cout << endl << endl;
@@ -41,7 +54,8 @@ int main(){
       cout << "Hromotluk muze aktivovat horlavost na meci hrdosti a kdyz uderi nepritele tak mu ubere 8hp a nepritel mu v pristim tahu ubere jakymkoliv utokem o 40% min hp(stoji 10 many)--(single attack)\n";
       cout << "Hromotluk muze aktivovat utok ktery utoci na vsechny nepritele v jeho blizkosti, tento utok ubere vsem nepretalum 5hp(stoji 3 many)--(area damage)\n";
       cout << "Hromotluk se muze ovsem behem hry naucit nove utoky ci vylepsit ty zakladni\n";
-      break;
+       rozdelovac();
+    break;
   case 3:
       cout << "vybrali jste postavu Gundolf";
       cout << endl << endl;
@@ -54,7 +68,8 @@ int main(){
       cout << "Gundolf muze jednou za tri kola aktivovat jed na kudle ktera ubira 4x4hp (stoji 4 many)--(single attack)\n";
       cout << "Gundolf muze jednou za ctyri kola aktivovat sekeru kterou muze utocit na vsechny nepratele v mistosti, a vsem ubere za 10hp(stoji 8many)--(area damage)\n";
       cout << "Gundolf se muze ovsem behem hry naucit nove utoky ci vylepsit zalkadni\n";
-      break;
+       rozdelovac();
+    break;
 
   default:
     cout<< "vyberte cislo v rozmezi 1-3";
@@ -67,8 +82,46 @@ int main(){
     cout << "spatne zadana hodnota!!"<< endl;
     }
  }while(spokojenost == 0);
- int zybtecnydotaz;
+ int zybtecnyDotaz;
   cout << "jsi pripraven zacit  hrat?\n";
-  cin >> zybtecnydotaz;
-  cout << "doufam ze ano jelikoz nemas na vyber\n" ;
+  cin >> zybtecnyDotaz;
+      rozdelovac();
+     cout << "doufam ze ano jelikoz nemas na vyber\n" ;
+
+     cout << "Vcera jsi byl v hospode a od tretiho dzbanu piva si nic nepamatujes. Tvym prvnim ukolem je zjistit co se vcera vse stalo.\n";
+     cout << "Probudil jsi se v lese na rozcesti.\n";
+     cout << "Mas u sebe 1 zlaty a 20 stribrnych (1 zlaty = 100 stribrnych), mas full hp a full manu.\n";
+      rozdelovac();
+ int rozcestiVyber=0;
+     cout << "1- muzes jit po ceste doprava smer mesto.\n";
+     cout << "2- muzes jit rovne smer bazina.\n";
+     cout << "3- muzes jit do leva smer cerne skaly.\n";
+     cin >> rozcestiVyber;
+      rozdelovac();
+     switch(rozcestiVyber){
+     case 1:
+int smer1;
+         cout << "Dosel jsi k hranicim mesta a straznici ti rekli ze vstup do mesta stoji 40 stribrnych. Co udelas?\n" ;
+         cout << "1- Zaplatis za vstup.\n";
+         cout << "2- Vratis se na rozcesti.\n";
+         cin >> smer1;
+        if (smer1 == 1){
+         cout << "Straze te pustili do mesta.\n";
+        }else if (smer1 ==2){
+          cout << "Jsi zpet na rozcesti.\n";
+          rozcesti();
+           }else {
+           cout << "Vyber z moznosti 1 nebo 2.\n";
+           }
+        break;
+     case 2:
+         //cout << ;
+         //cout << ;
+        break;
+     case 3:
+        //cout <<;
+        //cout <<;
+     default:
+         cout<< "vyberte cislo v rozmezi 1-3";
+     }
 }
